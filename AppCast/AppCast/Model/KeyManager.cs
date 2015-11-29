4bb9de7493584a0abcd945467d471193eafb18e5
auriz <ky.prodeathz@gmail.com>
2015-11-29 23:47:08 +0800

    Fix Meeting Form

---
+++ AppCast/AppCast/Model/KeyManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCast.Model
+{
+    class KeyManager
+    {
+        private int keyID;
+
+        public int KeyID
+        {
+            get { return keyID; }
+            set { keyID = value; }
+        }
+
+        private String symKey;
+
+        public String SymKey
+        {
+            get { return symKey; }
+            set { symKey = value; }
+        }
+
+        private String symKey2;
+
+        public String SymKey2
+        {
+            get { return symKey2; }
+            set { symKey2 = value; }
+        }
+    }
+}
---
+++ AppCast/AppCast/Model/Room.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCast.Model
+{
+    class Room
+    {
+        private int roomID;
+
+        public int RoomID
+        {
+            get { return roomID; }
+            set { roomID = value; }
+        }
+
+        private String roomTitle;
+
+        public String RoomTitle
+        {
+            get { return roomTitle; }
+            set { roomTitle = value; }
+        }
+
+        private String roomUser;
+
+        public String RoomUser
+        {
+            get { return roomUser; }
+            set { roomUser = value; }
+        }
+
+        private int keyID;
+
+        public int KeyID
+        {
+            get { return keyID; }
+            set { keyID = value; }
+        }
+
+        private DateTime createDate;
+
+        public DateTime CreateDate
+        {
+            get { return createDate; }
+            set { createDate = value; }
+        }
+
+    }
+}
