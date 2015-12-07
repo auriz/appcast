76218520593679e644410e3651565116cdbb2144
vekin2496 <kevin_how@live.com>
2015-12-07 09:42:51 +0800

    ftp upload download

---
+++ AppCast/AppCast/fileUpload.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AppCast
+{
+    partial class fileUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbUpload = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(45, 179);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(223, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Select File to Upload: ";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(265, 218);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(152, 47);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbUpload
+            // 
+            this.tbUpload.Location = new System.Drawing.Point(50, 221);
+            this.tbUpload.Name = "tbUpload";
+            this.tbUpload.Size = new System.Drawing.Size(199, 31);
+            this.tbUpload.TabIndex = 2;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.ForeColor = System.Drawing.Color.Green;
+            this.lblMsg.Location = new System.Drawing.Point(45, 271);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 25);
+            this.lblMsg.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 568);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.tbUpload);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "fileUpload";
+            this.Text = "fileUpload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbUpload;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AppCast/AppCast/fileUpload.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCast
+{
+    public partial class fileUpload : Form
+    {
+        public fileUpload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.AddExtension = true;
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    File.Copy(fileName, "D:\\Desktop\\TempServer\\Uploads\\" + Path.GetFileName(fileName));
+                    
+
+                    FtpWebRequest newRequest = (FtpWebRequest)WebRequest.Create("ftp://appcast-backend.cloudapp.net"+"/testingUpload.png");
+                    newRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                    //newRequest.UsePassive = false;
+
+                    newRequest.Credentials = new NetworkCredential("appcast","@ppcastPW1");
+
+                    Stream ftpStream = newRequest.GetRequestStream();
+                    FileStream file = File.OpenRead("D:\\Desktop\\TempServer\\Uploads\\" + Path.GetFileName(fileName));
+
+                    int length = 1024;
+                    byte[] buffer = new byte[length];
+                    int bytesRead = 0;
+
+                    do
+                    {
+                        bytesRead = file.Read(buffer, 0, length);
+                        ftpStream.Write(buffer, 0, bytesRead);
+                    }
+                    while(bytesRead !=0);
+
+                    file.Close();
+                    ftpStream.Close();
+
+                    lblMsg.Text += Path.GetFileName(fileName) + " has been successfully uploaded.\n";
+                    button1.Visible = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient request = new WebClient();
+            request.Credentials = new NetworkCredential("appcast", "@ppcastPW1");
+
+            byte[] filedata = request.DownloadData("ftp://appcast-backend.cloudapp.net" + "/testingUpload.png");
+
+            FileStream file = File.Create("D:\\Desktop\\Downloads\\testDownload.png");
+            file.Write(filedata, 0, filedata.Length);
+            file.Close();
+            MessageBox.Show("Download Complete.");
+ 
+ /*           FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://appcast-backend.cloudapp.net" + "/testingUpload.png");
+            request.KeepAlive = true;
+            request.UsePassive = false;
+            request.UseBinary = true;
+
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential("appcast", "@ppcastPW1");
+
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            using (Stream responseStream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(responseStream))
+            using (StreamWriter destination = new StreamWriter("D:\\Desktop\\Downloads\\testDownload.png"))
+            {
+                destination.Write(reader.ReadToEnd());
+                destination.Flush();
+            } 
+ */
+        }
+    }
+}
