4b309db31cf15acc5b785c85f195a43c9987fe44
auriz <ky.prodeathz@gmail.com>
2015-12-10 23:11:47 +0800

    Revert "Daniel"

---
+++ AppCast/AppCast/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Meeting meet = new Meeting();
+            meet.Show();
+            this.Hide();
+        }
+    }
+}
